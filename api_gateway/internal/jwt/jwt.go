3bb9a20ffebe2687a4dfbe2d58a256462482736d
MishkaBM <mixail_dremov@mail.ru>
2025-10-19 15:38:37 +0300

    add ValidateToken

--- api_gateway/internal/jwt/jwt.go
+++ api_gateway/internal/jwt/jwt.go
@@ -10,12 +10,13 @@ import (
 )
 
 type Claims struct {
-	ID   int      `json:"id"`
-	Role []string `json:"roles"`
+	ID    int      `json:"id"`
+	Role  []string `json:"roles"`
+	Email string   `json:"email"`
 	jwt.StandardClaims
 }
 
-func GenerateToken(id int, roles []string) (string, error) {
+func GenerateToken(id int, roles []string, email string) (string, error) {
 	err := godotenv.Load("api_gateway/.env")
 	if err != nil {
 		return "", err
@@ -27,15 +28,36 @@ func GenerateToken(id int, roles []string) (string, error) {
 		return "", fmt.Errorf("id must be greater more than 0")
 	}
 	Claims := &Claims{
-		ID:   id,
-		Role: roles,
+		ID:    id,
+		Role:  roles,
+		Email: email,
 		StandardClaims: jwt.StandardClaims{
 			ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
 			IssuedAt:  time.Now().Unix(),
 		},
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, Claims)
-	return token.SignedString([]byte(os.Getenv("SECRETKEY")))
+	return token.SignedString([]byte(os.Getenv("SECRET_KEY")))
+}
+
+func ValidateToken(tokenString string) (*Claims, error) {
+	if tokenString == "" {
+		return nil, fmt.Errorf("token is empty")
+	}
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, jwt.ErrSignatureInvalid
+		}
+		return []byte(os.Getenv("SECRET_KEY")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	claims, ok := token.Claims.(*Claims)
+	if ok && token.Valid {
+		return claims, nil
+	}
+	return nil, fmt.Errorf("token is invalid")
 }
 
 func HashedPassword(password string) (string, error) {
